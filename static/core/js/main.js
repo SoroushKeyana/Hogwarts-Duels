fe4fa29cb265ad34efefb9bb4ef29d75bff903a4
SoroushKeyana <soroush.keyana@gmail.com>
2025-09-15 10:59:40 +0200

    add house image to follow list

--- static/core/js/main.js
+++ static/core/js/main.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     results.innerHTML = '';
                     data.forEach(user => {
                         const li = document.createElement('li');
-                        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+                        li.className = 'list-group-item d-flex justify-content-between align-items-center search-list';
                         const userLink = document.createElement('a');
                         userLink.href = `/profile/${user.username}/`;
                         userLink.textContent = user.username;
@@ -20,20 +20,20 @@ document.addEventListener('DOMContentLoaded', () => {
                         const buttonDiv = document.createElement('div');
                         if (user.is_following) {
                             const unfollowButton = document.createElement('button');
-                            unfollowButton.className = 'btn btn-sm btn-danger';
+                            unfollowButton.className = 'btn btn-xs btn-danger';
                             unfollowButton.textContent = 'Unfollow';
                             unfollowButton.onclick = () => unfollow(user.id, unfollowButton);
                             buttonDiv.appendChild(unfollowButton);
                         } else {
                             const followButton = document.createElement('button');
-                            followButton.className = 'btn btn-sm btn-primary';
+                            followButton.className = 'btn btn-xs btn-primary';
                             followButton.textContent = 'Follow';
                             followButton.onclick = () => follow(user.id, followButton);
                             buttonDiv.appendChild(followButton);
                         }
 
                         const challengeButton = document.createElement('a');
-                        challengeButton.className = 'btn btn-sm btn-warning ms-2'; // Added ms-2 for margin-left
+                        challengeButton.className = 'btn btn-xs btn-warning ms-2'; 
                         challengeButton.textContent = 'Challenge';
                         challengeButton.href = `/duel/start/${user.id}/`;
                         buttonDiv.appendChild(challengeButton);
