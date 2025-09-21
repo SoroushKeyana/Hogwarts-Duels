6fadaf6a5ee43e552d10dfdb9475f09b1c2d3421
SoroushKeyana <soroush.keyana@gmail.com>
2025-09-21 15:46:26 +0200

    change allowed hosts

--- hogwarts_duels/settings.py
+++ hogwarts_duels/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='https://hogwarts-duels.onrender.com', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='hogwarts-duels.onrender.com,localhost,127.0.0.1', cast=Csv())
 
 
 # Application definition
